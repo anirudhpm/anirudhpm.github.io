5d3301e388135eeecddf8f323efaca4c68b7e2bd
Anirudh P M <anirudhpm@gmail.com>
2018-11-28 00:16:15 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+
+$('a.nav-item.nav-link').click(function(){
+    $('a.nav-item.nav-link').removeClass('active');
+    $(this).addClass('active');
+})
+
+$(window).mousewheel(function(event, delta) {
+    event.preventDefault();
+    var scrollx = $(window).scrollLeft();
+    $(window).scrollLeft(scrollx-(delta * 100));
+    });
+
+  function drawdio(){
+
+    var $pageContent = $('#content');
+    
+    
+    var loadContent = function(url) {
+        $pageContent.hide();
+     
+        $pageContent.load(url, function(){
+            $pageContent.fadeIn();
+            
+        });
+     
+      
+    };
+    loadContent('Drawdio.html');
+}
+    
+    
+    
+    
+    
